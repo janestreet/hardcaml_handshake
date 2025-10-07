015457c7d45acbea49d730fb4119c8aa1ef1a1e5
public-release <as-public-release@janestreet.com>
2025-10-07 02:42:24 +0100

    v0.18~preview.130.55+197

--- src/handshake.ml
+++ src/handshake.ml
@@ -22,6 +22,7 @@ type ('a, 'b) t =
 
 let id = Id
 let ( >>> ) a b = Compose (a, b)
+let of_list items = List.fold items ~init:id ~f:(fun acc x -> acc >>> x)
 let component f = Component f
 
 let build_pipeline_stages =
@@ -48,7 +49,7 @@ let run (components : _ t) (io : _ IO.t) =
   let pipeline_stages, data_dn = build_pipeline_stages components io.data in
   let ack_up =
     List.fold_right pipeline_stages ~init:io.ack ~f:(fun pipeline_stage ack ->
-      Signal.( <== ) pipeline_stage.ack_dn ack;
+      Signal.( <-- ) pipeline_stage.ack_dn ack;
       pipeline_stage.ack_up)
   in
   { IO.data = data_dn; ack = ack_up }
--- src/handshake.mli
+++ src/handshake.mli
@@ -14,21 +14,21 @@ val id : ('a, 'a) t
 
 (** Creates a handshake component with an explicit custom ack signal.
 
-    This function should be used to use regular hardcaml components within the
-    handshake framework.
-*)
+    This function should be used to use regular hardcaml components within the handshake
+    framework. *)
 val component : ('a IO.t -> 'b IO.t) -> ('a, 'b) t
 
 (** Creates a handshake arrow from a regular OCaml function.
 
-    This is useful for combinational logic that requires no ack signals.
-*)
+    This is useful for combinational logic that requires no ack signals. *)
 val arr : ('a -> 'b) -> ('a, 'b) t
 
 (** [>>> a b] composes two handshake components together. *)
 val ( >>> ) : ('a, 'b) t -> ('b, 'c) t -> ('a, 'c) t
 
+(** x[0] >>> x[1] >>> x[2] .... >>> x[n-1] *)
+val of_list : ('a, 'a) t list -> ('a, 'a) t
+
 (** Creates a chained handshake component comprising of an arbitrary number of components.
-    List of components can be empty, of which no handshake pipelining will be done.
-*)
+    List of components can be empty, of which no handshake pipelining will be done. *)
 val run : ('a, 'b) t -> 'a IO.t -> 'b IO.t
